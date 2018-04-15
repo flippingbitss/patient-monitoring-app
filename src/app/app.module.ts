17504c2d0440941c11d8048d838c330824298332
myotintaye <myotintaye00@gmail.com>
2018-04-14 20:21:17 -0400

    added core,shared,entry modules and login form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+ 
+import { AppComponent } from "./app.component";
 
-
-import { AppComponent } from './app.component';
-
+import { CoreModule } from "./core";
+import { SharedModule } from "./shared";
+import { AppRoutingModule } from "./app-routing.module";
+import { EntryModule } from "./entry/entry.module";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+
+    // core and shared
+    CoreModule,
+    SharedModule,
+
+    EntryModule,
+
+    AppRoutingModule,
   ],
+  declarations: [AppComponent],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
