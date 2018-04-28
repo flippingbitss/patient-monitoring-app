5bbeba9a0fd30bb7338ec1f2b4482a6fe58075e3
myotintaye <myotintaye00@gmail.com>
2018-04-28 15:54:09 -0400

    fixed build errors

--- src/app/secure/secure-home/secure-home.component.ts
+++ src/app/secure/secure-home/secure-home.component.ts
@@ -1,24 +1,27 @@
-import { Component, OnInit, ViewContainerRef } from '@angular/core';
-import {Router} from '@angular/router';
+import { Component, OnInit, ViewContainerRef } from "@angular/core";
+import { Router } from "@angular/router";
 
-import { NgxSmartModalService } from 'ngx-smart-modal';
-import { CreateTipComponent } from '@app/secure/create-tip/create-tip.component';
+import { NgxSmartModalService } from "ngx-smart-modal";
+import { CreateTipComponent } from "@app/secure/create-tip/create-tip.component";
 
 @Component({
-  selector: 'app-secure-home',
-  templateUrl: './secure-home.component.html',
-  styleUrls: ['./secure-home.component.scss']
+  selector: "app-secure-home",
+  templateUrl: "./secure-home.component.html",
+  styleUrls: ["./secure-home.component.scss"]
 })
-export class SecureHomeComponent implements OnInit  {
+export class SecureHomeComponent implements OnInit {
+  constructor(
+    private router: Router,
+    public ngxSmartModalService: NgxSmartModalService
+  ) {}
 
-  constructor(private router:Router,public ngxSmartModalService: NgxSmartModalService) { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  dialogInit() {}
+
+  viewDetail() {
+    this.router.navigate(["/patient-detail"]);
   }
 
-  dialogInit(){  }
-
-  viewDetail(){
-    this.router.navigate(['/patient-detail']);
-  }
-  }
+  loginClick() {}
+}
